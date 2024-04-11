635de8694b4870cf196e1f808746db8c9c8f3308
Jayakrishnan49 <166716494+Jayakrishnan49@users.noreply.github.com>
2024-04-11 21:11:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// -----------------img hover------------------
+function changeimg1(x,image)
+{
+    if(x==1){
+        image.src='images/1-0.webp'
+    }
+    if(x==2){
+        image.src='images/1.webp'
+    }
+}
+function changeimg2(x,image)
+{
+    if(x==1){
+        image.src='images/1-1.webp'
+    }
+    if(x==2){
+        image.src='images/2.webp'
+    }
+}
+function changeimg3(x,image)
+{
+    if(x==1){
+        image.src='images/1-2.webp'
+    }
+    if(x==2){
+        image.src='images/3.webp'
+    }
+}
+function changeimg4(x,image)
+{
+    if(x==1){
+        image.src='images/1-3.webp'
+    }
+    if(x==2){
+        image.src='images/4.webp'
+    }
+}
+function changeimg5(x,image)
+{
+    if(x==1){
+        image.src='images/1-4.webp'
+    }
+    if(x==2){
+        image.src='images/5.webp'
+    }
+}
+function changeimg6(x,image)
+{
+    if(x==1){
+        image.src='images/1-5.webp'
+    }
+    if(x==2){
+        image.src='images/6.webp'
+    }
+}
+function changeimg7(x,image)
+{
+    if(x==1){
+        image.src='images/1-6.webp'
+    }
+    if(x==2){
+        image.src='images/7.webp'
+    }
+}
+function changeimg8(x,image)
+{
+    if(x==1){
+        image.src='images/1-7.webp'
+    }
+    if(x==2){
+        image.src='images/8.webp'
+    }
+}
+function changeimg9(x,image)
+{
+    if(x==1){
+        image.src='images/1-8.webp'
+    }
+    if(x==2){
+        image.src='images/9.webp'
+    }
+}
